9402c3223ba2b510f7ef0987f21cd8555c19a037
Rohith Vishaal <rohith.vishaal@gmail.com>
2022-01-15 15:58:49 +0530

    for arm devices

---
+++ dongle_install_arm.sh
@@ -0,0 +1,31 @@
+sudo ln -s /lib/modules/$(uname -r)/build/arch/arm /lib/modules/$(uname -r)/build/arch/armv7l
+
+sudo apt-get install build-essential git dkms linux-headers-$(uname -r)
+
+git clone -b arm https://github.com/kelebek333/rtl8188fu rtl8188fu-arm
+
+sudo dkms add ./rtl8188fu-arm
+
+sudo dkms build rtl8188fu/1.0
+
+sudo dkms install rtl8188fu/1.0
+
+sudo cp ./rtl8188fu-arm/firmware/rtl8188fufw.bin /lib/firmware/rtlwifi/
+
+# Disable Power Management
+sudo mkdir -p /etc/modprobe.d/
+
+sudo touch /etc/modprobe.d/rtl8188fu.conf
+
+echo "options rtl8188fu rtw_power_mgnt=0 rtw_enusbss=0" | sudo tee /etc/modprobe.d/rtl8188fu.conf
+
+# Disable MAC Address spoofing
+sudo mkdir -p /etc/NetworkManager/conf.d/
+
+sudo touch /etc/NetworkManager/conf.d/disable-random-mac.conf
+
+echo -e "[device]\nwifi.scan-rand-mac-address=no" | sudo tee /etc/NetworkManager/conf.d/disable-random-mac.conf
+
+#If you are using kernel 5.15 and newer, you must create a configuration file with following commands for preventing
+#to conflict rtl8188fu module with built-in r8188eu module.
+echo 'alias usb:v0BDApF179d*dc*dsc*dp*icFFiscFFipFFin* rtl8188fu' | sudo tee /etc/modprobe.d/r8188eu-blacklist.conf
